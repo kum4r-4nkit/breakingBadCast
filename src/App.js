dd7f99cc0aece99127a135636a488ae61047ecf8
Ankit Kumar <ankitcs0026@gmail.com>
2022-04-01 23:21:31 +0530

    added M-UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.png'
 import axios from 'axios';
 import Spinner from './spinner.gif'
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import './App.css';
 
 const App = () => {
@@ -24,10 +28,24 @@ const App = () => {
       <header className='center'>
         <img src={logo} alt="" />
       </header>
-      <input
-        type='text' className='form-control' placeholder='Search characters' value={query}
-        onChange={(e) => setQuery(e.target.value)} autoFocus
-      />
+      <Box
+        component="form"
+        sx={{
+          margin: 'auto',
+          marginTop: 2,
+          marginBottom: 6,
+          maxWidth: 600,
+          backgroundColor: '#e8f5e9',
+          '&:hover': {
+            backgroundColor: '#ffffff'
+          },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField fullWidth id="filled-basic" variant="filled" label="Search characters" value={query}
+          onChange={(e) => setQuery(e.target.value)} autoFocus />
+      </Box>
       {isLoading ? (
         <img src={Spinner}
           style={{ width: '120px', margin: 'auto', display: 'block' }}
@@ -35,9 +53,18 @@ const App = () => {
         />
       ) : (
         <section className='cards'>
-          {items.map((item) => (
-            <div className='card'>
-              <img src={item.img} alt='' />
+          {items.map((item, i) => (
+            <div key={i}>
+              <a href={`https://en.wikipedia.org/wiki/${item.name}_(Breaking_Bad)`} target="_blank" rel="noreferrer">
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    component="img"
+                    alt="green iguana"
+                    height="300"
+                    image={item.img}
+                  />
+                </Card>
+              </a>
             </div>
           ))}
         </section>
@@ -45,5 +72,4 @@ const App = () => {
     </div>
   )
 }
-
 export default App
